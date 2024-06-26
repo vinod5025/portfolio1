485f115b4e37c3bbeafef38fe15b4f122f0d16fb
vinod5025 <168267427+vinod5025@users.noreply.github.com>
2024-06-26 22:06:47 +0530

    Update user_route.js

--- routes/user_route.js
+++ routes/user_route.js
@@ -6,7 +6,7 @@ router.get("/",async function(req,res){
     var about=await exe(`SELECT * FROM about_page`);
     var certificate=await exe(`SELECT * FROM certificate_page`);
     var project=await exe(`SELECT * FROM project_page`);
-    var count=await exe(`SELECT * FROM  count_section `);
+    var count=await exe(`SELECT * FROM  count_section `); 
     var client_list=await exe(`SELECT * FROM client_list`);
     var contact_self=await exe(`SELECT * FROM contact_myself`);
     var obj={"home":data[0],"about":about[0],"certificate":certificate[0],"project":project[0],"count":count[0],"client_list":client_list[0],"contact_self":contact_self[0]};
